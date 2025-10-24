03491c5963c2186d0569a585bd2cdc09554e748f
eliott <eliott.barrois@edu.ece.fr>
2025-10-24 14:33:47 +0200

    feat: add rectangle library

---
+++ rectangle.h
@@ -0,0 +1,23 @@
+//
+// Created by eliot on 24/10/2025.
+//
+
+#ifndef TP_FONCTIONS_RECTANGLE_H
+#define TP_FONCTIONS_RECTANGLE_H
+#define RECTANGLE 1
+
+/**
+ *
+ */
+void Rectangle(void);
+
+/**
+ *
+ * @param phauteur
+ * @param plargeur
+ */
+void initRectangle(int* phauteur, int* plargeur);
+void afficherRectangle(int hauteur, int largeur);
+int aireRectangle(int hauteur, int largeur);
+
+#endif //TP_FONCTIONS_RECTANGLE_H
---
+++ triangle.h
@@ -0,0 +1,8 @@
+//
+// Created by eliot on 24/10/2025.
+//
+
+#ifndef TP_FONCTIONS_TRIANGLE_H
+#define TP_FONCTIONS_TRIANGLE_H
+
+#endif //TP_FONCTIONS_TRIANGLE_H
